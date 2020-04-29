dd212f7b39e3f70c6184e8129efe25fcd112851d
salvafern <54405067+salvafern@users.noreply.github.com>
2020-04-29 10:16:03 +0200

    Relative path to data.csv added as a variable

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,17 +4,16 @@ import pandas as pd
 # Read data from wfs request
 dataf = pd.read_csv("{{cookiecutter.data_wfs_request}}")
 
+# Get path to file
+path = os.path.join(os.getcwd(), "data", "raw_data",'data.csv')
+
 # Save data in raw_data directory
-dataf.to_csv(os.path.join(os.getcwd(), "data", "raw_data",'data.csv'), encoding='utf-8')
+dataf.to_csv(path, encoding='utf-8')
 
 # Check if file was downloaded.
 try:
-    path = os.path.join(os.getcwd(), "data", "raw_data", "data.csv")
     f = open(path)
 except IOError:
-    raise FileNotFoundError("An error has occured. Please send Traceback to bio@emodnet.eu")
+    raise FileNotFoundError("An error has occured while downloading the data. Please send Traceback to bio@emodnet.eu")
 else: 
     print("Data successfully downloaded from https://emodnet-biology.eu/ in: " + path)
-
-
-
